2b5d8d2699eb30de2d99fe8c7e94b2bb37d52b33
username <username@domain.example>
2023-12-08 19:01:40 +0100

    idk

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,56 +1,74 @@
 //
 // Created by Fujitsu on 04.12.2023.
 //
-#include <cstdlib>
-#include <set>
-#include "types.hpp"
+#ifndef NETSIM_STORAGE_TYPES_HPP
+#define NETSIM_STORAGE_TYPES_HPP
+
 #include "package.hpp"
+#include <list>
 
-#ifndef SERWERYAIR_STORAGE_TYPES_HPP
-#define SERWERYAIR_STORAGE_TYPES_HPP
-
-
-enum class PackageQueueType{
+enum class PackageQueueType {
     FIFO,
     LIFO
 };
 
-
-
-
-template<typename T>;
-
 class IPackageStockpile {
 public:
-    using const_iterator = typename T::const_iterator;
+    using const_iterator = std::list<Package>::const_iterator;
 
-    virtual void push(T&& package) = 0;
-    virtual bool empty() const = 0;
+    virtual void push(Package&& package) = 0;
+
     virtual std::size_t size() const = 0;
 
+    virtual bool empty() const = 0;
+
     virtual const_iterator cbegin() const = 0;
+
     virtual const_iterator cend() const = 0;
+
     virtual const_iterator begin() const = 0;
+
     virtual const_iterator end() const = 0;
 
     virtual ~IPackageStockpile() = default;
 };
 
-class IPackageQueue : public IPackageStockpile<Package> {
+class IPackageQueue : public IPackageStockpile {
 public:
     virtual Package pop() = 0;
+
     virtual PackageQueueType get_queue_type() const = 0;
 
     ~IPackageQueue() override = default;
 };
 
-class PackageQueue {
+class PackageQueue : public IPackageQueue {
 public:
-    PackageQueue(PackageQueueType) = default;
+    explicit PackageQueue(PackageQueueType queue_type) : queue_(), queue_type_(queue_type) {}
 
-    ~PackageQueue(PackageQueueType) = default;
+    void push(Package&& package) override { queue_.emplace_back(std::move(package)); }
 
+    std::size_t size() const override { return queue_.size(); }
+
+    bool empty() const override { return queue_.empty(); }
+
+    const_iterator cbegin() const override { return queue_.cbegin(); }
+
+    const_iterator cend() const override { return queue_.cend(); }
+
+    const_iterator begin() const override { return queue_.cbegin(); }
+
+    const_iterator end() const override { return queue_.cend(); }
+
+    Package pop() override;
+
+    PackageQueueType get_queue_type() const override { return queue_type_; }
+
+    ~PackageQueue() override = default;
 private:
-}
+    std::list<Package> queue_;
+    PackageQueueType queue_type_;
+};
 
-#endif //SERWERYAIR_STORAGE_TYPES_HPP
+#endif //NETSIM_STORAGE_TYPES_HPP
+
